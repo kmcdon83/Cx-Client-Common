59c29c05b929cdd1995a1a287759ea7c8516b586
AlexeyK <alexey.kononov@checkmarx.com>
2019-11-25 16:22:58 +0200

    Added a test for SCA scan.

--- src/test/java/com/cx/restclient/connection/ProjectScanTests.java
+++ src/test/java/com/cx/restclient/connection/ProjectScanTests.java
@@ -6,6 +6,7 @@ import com.cx.restclient.dto.DependencyScanResults;
 import com.cx.restclient.dto.DependencyScannerType;
 import com.cx.restclient.exception.CxClientException;
 import com.cx.restclient.sast.dto.SASTResults;
+import com.cx.restclient.sca.dto.SCAConfig;
 import com.cx.utility.TestingUtils;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -41,9 +42,10 @@ public class ProjectScanTests {
             client.init();
             client.createDependencyScan();
             client.waitForDependencyScanResults();
-            final DependencyScanResults latestOSAResults = client.getLatestDependencyScanResults();
-            Assert.assertNotNull(latestOSAResults.getOsaResults());
-            Assert.assertNotNull("Expected valid osa scan id", latestOSAResults.getOsaResults().getOsaScanId());
+            final DependencyScanResults results = client.getLatestDependencyScanResults();
+            Assert.assertNull(results.getScaResults());
+            Assert.assertNotNull(results.getOsaResults());
+            Assert.assertNotNull("Expected valid osa scan id", results.getOsaResults().getOsaScanId());
         } catch (IOException | CxClientException e) {
             e.printStackTrace();
             log.error("Error running  osa scan: " + e.getMessage());
@@ -59,11 +61,30 @@ public class ProjectScanTests {
             client.init();
             client.createSASTScan();
             client.waitForSASTResults();
-            final SASTResults latestSASTResults = client.getLatestSASTResults();
+            SASTResults latestSASTResults = client.getLatestSASTResults();
             Assert.assertNotEquals("Expected valid SAST scan id", 0, latestSASTResults.getScanId());
         } catch (IOException | CxClientException | InterruptedException e) {
             e.printStackTrace();
             log.error("Error running sast scan: " + e.getMessage());
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void runScaScan() throws MalformedURLException, CxClientException {
+        CxScanConfig config = initScaConfig();
+        client = new CxShragaClient(config, log);
+        try {
+            client.init();
+            client.createDependencyScan();
+            DependencyScanResults results = client.waitForDependencyScanResults();
+            Assert.assertNotNull(results);
+            Assert.assertNull(results.getOsaResults());
+            Assert.assertNotNull(results.getScaResults());
+            Assert.assertNotNull(results.getScaResults().getSummary());
+            Assert.assertNotNull(results.getScaResults().getScanId());
+        } catch (Exception e) {
+            log.error("Error running SCA scan: " + e);
             Assert.fail(e.getMessage());
         }
     }
@@ -73,7 +94,7 @@ public class ProjectScanTests {
         config.setSastEnabled(true);
         config.setReportsDir(new File("C:\\report"));
         config.setSourceDir(props.getProperty("sastSource"));
-        config.setUsername(props.getProperty("user"));
+        config.setUsername(props.getProperty("username"));
         config.setPassword(props.getProperty("password"));
         config.setUrl(props.getProperty("serverUrl"));
         config.setCxOrigin("common");
@@ -93,12 +114,12 @@ public class ProjectScanTests {
         CxScanConfig config = new CxScanConfig();
         config.setDependencyScannerType(DependencyScannerType.OSA);
         config.setSastEnabled(false);
-        config.setSourceDir(props.getProperty("osaSource"));
+        config.setSourceDir(props.getProperty("dependencyScanSourceDir"));
         config.setReportsDir(new File("C:\\report"));
-        config.setUsername(props.getProperty("user"));
+        config.setUrl(props.getProperty("serverUrl"));
+        config.setUsername(props.getProperty("username"));
         config.setPassword(props.getProperty("password"));
 
-        config.setUrl(props.getProperty("serverUrl"));
         config.setCxOrigin("common");
         config.setProjectName("osaOnlyScan");
         config.setPresetName("Default");
@@ -113,4 +134,30 @@ public class ProjectScanTests {
         return config;
     }
 
+    private CxScanConfig initScaConfig() {
+        CxScanConfig config = new CxScanConfig();
+        config.setUrl(props.getProperty("serverUrl"));
+        config.setUsername(props.getProperty("username"));
+        config.setPassword(props.getProperty("password"));
+        config.setDependencyScannerType(DependencyScannerType.SCA);
+        config.setSastEnabled(false);
+        config.setSourceDir(props.getProperty("dependencyScanSourceDir"));
+        config.setOsaThresholdsEnabled(true);
+        config.setProjectName("scaOnlyScan");
+        config.setTeamPath("\\CxServer");
+
+        // Disabling certificate validation, otherwise we'll get an error during SCA login.
+        // TODO: fix HTTPS logic in CxHttpClient.
+        config.setDisableCertificateValidation(true);
+
+        SCAConfig sca = new SCAConfig();
+        sca.setApiUrl("https://api.lumodev.com");
+        sca.setAccessControlUrl("https://upgrade.dev-ac-checkmarx.com");
+        sca.setTenant("Checkmarx");
+        sca.setUsername(props.getProperty("sca.username"));
+        sca.setPassword(props.getProperty("sca.password"));
+        config.setScaConfig(sca);
+
+        return config;
+    }
 }
