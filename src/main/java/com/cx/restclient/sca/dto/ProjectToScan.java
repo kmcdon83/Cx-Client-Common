06c66e36d679671419bb1655ed3e05bf0ff72595
AlexeyK <alexey.kononov@checkmarx.com>
2020-04-26 16:42:21 +0300

    Starting the scan.

---
+++ src/main/java/com/cx/restclient/sca/dto/ProjectToScan.java
@@ -0,0 +1,12 @@
+package com.cx.restclient.sca.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ProjectToScan {
+    private String id;
+    private String type;
+    private UploadHandler handler;
+}
