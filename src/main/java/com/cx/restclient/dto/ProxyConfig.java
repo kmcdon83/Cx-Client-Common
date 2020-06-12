7f261071c8ecd75ff4529c2d2731251b8cda78eb
ilandn <ilandayans@gmail.com>
2020-06-12 14:00:24 -0500

    bugid: Added constructor - ProxyConfig CR_by: n/a

--- src/main/java/com/cx/restclient/dto/ProxyConfig.java
+++ src/main/java/com/cx/restclient/dto/ProxyConfig.java
@@ -3,11 +3,23 @@ package com.cx.restclient.dto;
 import java.io.Serializable;
 
 public class ProxyConfig implements Serializable {
+
     private String host;
     private int port;
     private String username;
     private String password;
     private boolean useHttps;
+
+    public ProxyConfig() {
+    }
+
+    public ProxyConfig(String host, int port, String username, String password, boolean useHttps) {
+        this.host = host;
+        this.port = port;
+        this.username = username;
+        this.password = password;
+        this.useHttps = useHttps;
+    }
 
     public String getHost() {
         return host;
